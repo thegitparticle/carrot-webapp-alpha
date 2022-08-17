2cf719e4e7c1cc229334c698c9fedf6682c8605d
Set <12975157+thegitparticle@users.noreply.github.com>
2022-08-17 09:50:26 +0530

    cleanup 1

--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,6 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import HomePage from "./HomePage";
 import React, { useMemo } from "react";
-import {
-	ConnectionProvider,
-	WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-	CoinbaseWalletAdapter,
-	GlowWalletAdapter,
-	PhantomWalletAdapter,
-	SlopeWalletAdapter,
-	SolflareWalletAdapter,
-	SolletExtensionWalletAdapter,
-	SolletWalletAdapter,
-	TorusWalletAdapter,
-	TokenaryWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-import {
-	WalletModalProvider,
-	WalletDisconnectButton,
-	WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
-import {
-	createDefaultAuthorizationResultCache,
-	SolanaMobileWalletAdapter,
-} from "@solana-mobile/wallet-adapter-mobile";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 export default function Home() {
