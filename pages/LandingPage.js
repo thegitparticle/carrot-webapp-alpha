a1a1f7f72f5c5fd924bd26c0a3da28ee0a191c26
Set <12975157+thegitparticle@users.noreply.github.com>
2022-08-21 13:46:14 +0530

    landing page improvements - Carrot logo, copy

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import Link from "next/link";
 import Image from "next/image";
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 function LandingPage() {
 	function Header() {
@@ -26,12 +27,17 @@ function LandingPage() {
 		return (
 			<div className="flex w-full sm:h-screen flex-col-reverse sm:flex-row justify-center items-center snap-start">
 				<div className="flex w-5/6 h-4/6 sm:h-screen relative flex-col items-center justify-center mt-4 mb-32 sm:mt-0 sm:mb-0">
-					<p className="text-orange-500 font-display font-black text-7xl flex my-4">
-						Carrot 🥕
-					</p>
+					<div className="flex my-4">
+						<Image
+							src="/Carrot_logo_themed.svg"
+							width={228}
+							height={89}
+							objectFit="fixed"
+						/>
+					</div>
 					<p className="text-layout-100 font-display font-bold text-xl flex-row flex my-4">
 						Earn
-						<p className="text-orange-500 font-black">
+						<p className="text-carrot_orange-500 font-black">
 							&nbsp; loyalty score NFTs &nbsp;
 						</p>
 						automatically from your fav brands!
@@ -39,17 +45,20 @@ function LandingPage() {
 					<p className="text-layout-100 font-display font-bold text-xl flex-row flex my-4">
 						Just pay using Solana Pay & come here to mint them.
 					</p>
+					<p className="text-layout-100 font-display font-bold text-xl flex-row flex my-4">
+						Coming soon to Solana Mainnet.
+					</p>
 				</div>
 				<div className="flex w-5/6 h-screen relative items-center justify-center my-6 sm:my-0 flex-row">
 					<div className="flex flex-col items-center">
-						<div className="transition ease-in-out delay-150 rotate-12 translate-y-24 hover:translate-y-20 hover:scale-110 hover:rotate-0">
+						<div className="transition ease-in-out delay-150 rotate-10 translate-y-24 hover:translate-y-20 hover:scale-110 hover:rotate-0">
 							<Image
 								src="/nftcards1.png"
 								width={342}
 								height={400}
 							/>
 						</div>
-						<div className="transition ease-in-out delay-150 -rotate-12 hover:-translate-y-1 hover:scale-110 hover:rotate-0">
+						<div className="transition ease-in-out delay-150 -rotate-8 hover:-translate-y-1 hover:scale-110 hover:rotate-0">
 							<Image
 								src="/nftcards2.png"
 								width={342}
@@ -57,7 +66,7 @@ function LandingPage() {
 							/>
 						</div>
 					</div>
-					<div className="transition ease-in-out delay-150 -rotate-45 -translate-x-24 hover:-translate-y-1 hover:scale-110 hover:rotate-12">
+					<div className="transition ease-in-out delay-150 -rotate-24 -translate-x-24 hover:-translate-y-1 hover:scale-110 hover:rotate-0">
 						<Image src="/nftcards3.png" width={342} height={400} />
 					</div>
 				</div>
